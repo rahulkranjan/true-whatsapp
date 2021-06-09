4610657b3e49b43c17cd2cbd801ced73bf07d455
Rahul Kumar Ranjan <kjrahul21@gmail.com>
2021-06-09 23:23:10 +0530

    sd

---
+++ script.js
@@ -0,0 +1,6 @@
+{/* <script type='text/javascript'> */}
+    function clickyClick() {
+      url = 'http://wa.me/91' + document.getElementById("comment").value
+      window.open(url, '_blank');
+    }
+//   </script>
