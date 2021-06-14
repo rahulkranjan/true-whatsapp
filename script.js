4fda53a27006ea68bbd7821b523aa233596497bd
Rahul Kumar Ranjan <kjrahul21@gmail.com>
2021-06-14 16:23:28 +0530

    df

--- script.js
+++
@@ -1,6 +0,0 @@
-{/* <script type='text/javascript'> */}
-    function clickyClick() {
-      url = 'http://wa.me/91' + document.getElementById("comment").value
-      window.open(url, '_blank');
-    }
-//   </script>
